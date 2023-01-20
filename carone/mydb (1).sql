aff8cd84d28025abd3fa0ba47b4f5eec496474e6
Addy3131 <adnankureshi15@gmail.com>
2023-01-20 13:16:34 +0530

    change

--- carone/mydb (1).sql
+++ carone/mydb (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2023 at 12:42 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Jan 20, 2023 at 08:45 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,7 @@ CREATE TABLE `admin` (
   `pass` varchar(255) NOT NULL,
   `created_dt` datetime NOT NULL,
   `updated_dt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admin`
@@ -56,15 +56,14 @@ CREATE TABLE `advertisment` (
   `car_number` int(11) DEFAULT NULL,
   `rc_book` varchar(255) DEFAULT NULL,
   `licence_no` int(11) DEFAULT NULL,
-  `car_pickup_address` varchar(255) DEFAULT NULL,
-  `fuel_type` varchar(255) DEFAULT NULL,
-  `rent_type` varchar(255) DEFAULT NULL,
+  `car_img` varchar(255) DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
-  `cust_id` int(11) DEFAULT NULL,
-  `city_id` int(11) DEFAULT NULL,
-  `area_id` int(11) DEFAULT NULL,
-  `cate_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `fuel_type` varchar(255) NOT NULL,
+  `cus_id` int(11) DEFAULT NULL,
+  `city` int(11) DEFAULT NULL,
+  `area` int(11) DEFAULT NULL,
+  `cat` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -78,7 +77,18 @@ CREATE TABLE `area` (
   `created_dt` datetime DEFAULT NULL,
   `update_dt` datetime DEFAULT NULL,
   `city_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `area`
+--
+
+INSERT INTO `area` (`area_id`, `area_name`, `created_dt`, `update_dt`, `city_id`) VALUES
+(1, 'Maninagar', NULL, NULL, 1),
+(2, 'Naroda', NULL, NULL, 1),
+(3, 'Jashodanagar', NULL, NULL, 1),
+(4, 'Borivali', NULL, NULL, 2),
+(5, 'Virar', NULL, NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -95,7 +105,7 @@ CREATE TABLE `booking` (
   `update_dt` datetime DEFAULT NULL,
   `cust_id` int(11) DEFAULT NULL,
   `adver_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -106,10 +116,20 @@ CREATE TABLE `booking` (
 CREATE TABLE `categories` (
   `cat_id` int(11) NOT NULL,
   `cat_name` varchar(255) DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
+  `cat_img` varchar(255) DEFAULT NULL,
   `created_dt` datetime DEFAULT NULL,
   `updated_dt` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_img`, `created_dt`, `updated_dt`) VALUES
+(2, 'Sedan', 'sedan.jpg', NULL, NULL),
+(15, 'SUV', 'suv.jpeg', NULL, NULL),
+(16, 'Coupe', 'coupe.jpg', NULL, NULL),
+(17, ' SPORTS CAR', 'sports.jpg', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -122,7 +142,7 @@ CREATE TABLE `city` (
   `city_name` varchar(255) DEFAULT NULL,
   `created_dt` datetime DEFAULT NULL,
   `update_dt` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `city`
@@ -132,7 +152,12 @@ INSERT INTO `city` (`city_id`, `city_name`, `created_dt`, `update_dt`) VALUES
 (1, 'Ahmedabad', NULL, NULL),
 (2, 'Mumbai', NULL, NULL),
 (3, 'Chennai', NULL, NULL),
-(4, 'Bangalore', NULL, NULL);
+(4, 'Bangalore', NULL, NULL),
+(5, 'Delhi', NULL, NULL),
+(6, 'Goa', NULL, NULL),
+(7, 'Kolkata', NULL, NULL),
+(8, 'Lucknow', NULL, NULL),
+(9, 'Mirzapur', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -143,7 +168,7 @@ INSERT INTO `city` (`city_id`, `city_name`, `created_dt`, `update_dt`) VALUES
 CREATE TABLE `contact` (
   `contact_us` int(11) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -152,13 +177,20 @@ CREATE TABLE `contact` (
 --
 
 CREATE TABLE `employee` (
-  `employee_id` int(11) NOT NULL,
+  `emp_id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` int(11) DEFAULT NULL,
+  `pass` int(11) DEFAULT NULL,
   `created_dt` datetime DEFAULT NULL,
   `update_dt` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emp_id`, `name`, `email`, `pass`, `created_dt`, `update_dt`) VALUES
+(2, 'Adnan ', 'Adnan@gmail.com', 81, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -171,7 +203,7 @@ CREATE TABLE `rent_dec` (
   `Tandc` varchar(255) DEFAULT NULL,
   `created_dt` datetime DEFAULT NULL,
   `update_dt` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -192,15 +224,14 @@ CREATE TABLE `user` (
   `created_dt` datetime DEFAULT NULL,
   `updated_dt` datetime DEFAULT NULL,
   `city_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`cus_id`, `cus_name`, `cemail`, `cpass`, `cphone`, `cgen`, `city`, `file`, `status`, `created_dt`, `updated_dt`, `city_name`) VALUES
-(2, 'addy', 'addy@GMAIL.COM', '81dc9bdb52d04dc20036dbd8313ed055', 1234567890, 'male', 2, '', 'unblock', '2023-01-19 14:48:24', '2023-01-19 14:48:24', ''),
-(5, 'RAJ', 'raj@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1234567689, 'Female', 3, 'download.jfif', 'unblock', '2023-01-19 15:11:05', '2023-01-19 15:11:05', '');
+(6, 'Addy', 'addy@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 123456789, 'Male', 9, 'LOGIN FORM.png', 'unblock', '2023-01-19 20:18:47', '2023-01-19 20:18:47', '');
 
 --
 -- Indexes for dumped tables
@@ -217,10 +248,10 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `advertisment`
   ADD PRIMARY KEY (`adv_id`),
-  ADD KEY `cust_id` (`cust_id`),
-  ADD KEY `city_id` (`city_id`),
-  ADD KEY `area_id` (`area_id`),
-  ADD KEY `cate_id` (`cate_id`);
+  ADD KEY `cust_id` (`cus_id`),
+  ADD KEY `city_id` (`city`),
+  ADD KEY `area_id` (`area`),
+  ADD KEY `cate_id` (`cat`);
 
 --
 -- Indexes for table `area`
@@ -247,7 +278,14 @@ ALTER TABLE `categories`
 -- Indexes for table `city`
 --
 ALTER TABLE `city`
-  ADD PRIMARY KEY (`city_id`);
+  ADD PRIMARY KEY (`city_id`),
+  ADD UNIQUE KEY `city_name` (`city_name`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`emp_id`);
 
 --
 -- Indexes for table `user`
@@ -276,7 +314,7 @@ ALTER TABLE `advertisment`
 -- AUTO_INCREMENT for table `area`
 --
 ALTER TABLE `area`
-  MODIFY `area_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `area_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `booking`
@@ -288,19 +326,25 @@ ALTER TABLE `booking`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -310,10 +354,10 @@ ALTER TABLE `user`
 -- Constraints for table `advertisment`
 --
 ALTER TABLE `advertisment`
-  ADD CONSTRAINT `advertisment_ibfk_1` FOREIGN KEY (`cust_id`) REFERENCES `user` (`cus_id`),
-  ADD CONSTRAINT `advertisment_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`),
-  ADD CONSTRAINT `advertisment_ibfk_3` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`),
-  ADD CONSTRAINT `advertisment_ibfk_4` FOREIGN KEY (`cate_id`) REFERENCES `categories` (`cat_id`);
+  ADD CONSTRAINT `advertisment_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `user` (`cus_id`),
+  ADD CONSTRAINT `advertisment_ibfk_2` FOREIGN KEY (`city`) REFERENCES `city` (`city_id`),
+  ADD CONSTRAINT `advertisment_ibfk_3` FOREIGN KEY (`area`) REFERENCES `area` (`area_id`),
+  ADD CONSTRAINT `advertisment_ibfk_4` FOREIGN KEY (`cat`) REFERENCES `categories` (`cat_id`);
 
 --
 -- Constraints for table `area`
